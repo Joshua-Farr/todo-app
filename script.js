e43f6211e946b1bdb04774167aa34dbdca4ba63a
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-06-30 16:51:30 -0500

    Added additional JavaScript functionality (delete)

--- script.js
+++ script.js
@@ -3,55 +3,82 @@ const todoList = document.getElementById("todo-list");
 const completeBtn = document.getElementById("select-item-btn");
 
 
-
-
-
-let todoListArray = []
-let numItemsLeftToDo = null;
+let todoListArray = [];
+let numItemsLeftToDo = 0;
 
 inputFieldText.addEventListener("keydown", function(e){
     if(e.key === "Enter" && inputFieldText.value != ""){
         addTodoItem(inputFieldText.value);
         clearInputField();
-        renderTodoList(todoListArray);
         updateNumLeft();
+
     }
 });
 
 
-
-console.log("Button exists!")
-completeBtn.addEventListener("click", function(){
-    console.log("Completed!!");
+document.addEventListener("click", function(e){
+    if(e.target.id === "select-item-btn"){
+        console.log("Button selected!");
+        e.target.classList.toggle("completed");
+    } else if(e.target.id === "delete-btn"){
+        console.log("Button deleted!");
+        e.target.parentElement.remove();
+        numItemsLeftToDo--;
+        updateNumLeft();
+    }
 })
 
 
+
 function addTodoItem(todoItem){
-        todoListArray.unshift(
-            { 
-                html: 
-                    `<li class="to-do-item">
-                        <button class="select-item-btn" id="select-item-btn"></button>
-                        <p>${todoItem}</p>
-                        <img src="./images/icon-cross.svg" class="x" alt="cancel">
-                    </li>`,
-                done: false
-            });
+    todoList.innerHTML += `<li class="to-do-item">
+                                <button class="select-item-btn" id="select-item-btn"></button>
+                                <p>${todoItem}</p>
+                                <img src="./images/icon-cross.svg" alt="delete" id="delete-btn">
+                            </li>`
+    numItemsLeftToDo++;
 }
+
+
+// console.log("Button exists!")
+// completeBtn.addEventListener("click", function(){
+//     console.log("Completed!!");
+// })
+
+// function addTodoItem(todoItem){
+//     todoListArray.unshift(
+//         { 
+//             html: 
+//                 `<li class="to-do-item">
+//                     <button class="select-item-btn" id="select-item-btn"></button>
+//                     <p>${todoItem}</p>
+//                     <img src="./images/icon-cross.svg" alt="delete" id="delete-btn">
+
+//                 </li>`,
+//             done: false
+//         });
+// }
 
 function clearInputField(){
     inputFieldText.value = "";
 }
 
-function renderTodoList(todoListArr){
-    todoList.innerHTML = "";
-    todoListArr.forEach(function(todo){
-        todoList.innerHTML += todo.html;
-    })
+// function renderTodoList(todoListArr){
+//     todoList.innerHTML = "";
+//     todoListArr.forEach(function(todo){
+//         todoList.innerHTML += todo.html;
+//     })
+// }
+
+
+function addFunctionality(listitem){
+    const closeBtn = listItem.querySelectorAll(".close");
+    closeBtn.addEventListener("click",  )
+
 }
 
 function updateNumLeft(){
-    document.getElementById("num-items-left").textContent = todoListArray.length;
+    document.getElementById("num-items-left").textContent = numItemsLeftToDo;
 }
 
 function markAsCompleted(todoItem){
